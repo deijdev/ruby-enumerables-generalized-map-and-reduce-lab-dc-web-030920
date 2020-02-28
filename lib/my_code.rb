a436508f1e3d6950af1921be50f9327a3f1feecd
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 06:33:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,25 +1,24 @@
 def map(array)
-  new=[]
-  i=0 
-  
-  while i< array.length 
+  new = []
+  i = 0
+  while i < array.length
   new.push(yield(array[i]))
-  i+=1 
-end
-new
+  i += 1
+  end
+  new
 end
 
 def reduce(array, sv=nil)
   if sv
-    sum=sv
-    i=0 
-  else
-    sum = array[0]
-    i=1 
+    sum = sv
+    i = 0
+    else 
+      sum = array[0]
+      i = 1
   end
-  while i < array.length 
-  sum = yield(sum(array[i]))
-  i += 1 
-end
-sum
+  while i < array.length
+  sum = yield(sum, array[i])
+  i += 1
+  end
+  sum
 end
